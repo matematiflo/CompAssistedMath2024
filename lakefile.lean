d8fa528538138a51458d8f0a5c18e9ae0155b865
C0MEDY <mishaprudnikov11@gmail.com>
2024-07-13 12:06:09 +0200

    .

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ package «leanproject» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "latest-release"
 
 require mdgen from git
   "https://github.com/Seasawher/mdgen" @ "main"
